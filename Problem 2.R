48c5e75ce1f65254c9f30c9f089c890ba5ed40eb
nskaue <nskaue@hotmail.no>
2021-03-08 11:17:56 +0100

    Labels plots 2a

--- Problem 2.R
+++ Problem 2.R
@@ -7,11 +7,16 @@ prob = read.table("obsprob.txt", header = TRUE)
 
 obs.matrix = matrix(obs$N_obs, byrow = FALSE, nrow = sqrt(nrow(obs)))
 
-image.plot(x = seq(5, 295, by = 10), y = seq(5, 295, by = 10), z = obs.matrix)
+image.plot(x = seq(5, 295, by = 10), y = seq(5, 295, by = 10), z = obs.matrix, 
+           main = "The number of pine trees observed in each grid unit",
+           xlab = "X", ylab = "Y", legend)
 
 prob.matrix = matrix(prob$alpha, byrow = FALSE, nrow = sqrt(nrow(prob)))
 
-image.plot(x = seq(5, 295, by = 10), y = seq(5, 295, by = 10), z = prob.matrix)
+image.plot(x = seq(5, 295, by = 10), y = seq(5, 295, by = 10), z = prob.matrix,
+           main = "Probability of observing a pine tree occuring in the grid",
+           xlab = "X", ylab = "Y")
+
 
 # kommenter: sammenheng mellom plots
 
