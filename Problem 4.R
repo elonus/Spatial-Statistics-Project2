3e50d4541f21c59f4e7a6a9ecb301aea3a26c7d4
nskaue <nskaue@hotmail.no>
2021-03-19 12:32:25 +0100

    Start på oppgave 4

--- Problem 4.R
+++ Problem 4.R
@@ -2,5 +2,25 @@ library(MASS)
 library(spatstat)
 library(spatial)
 
-k = runif()
+set.seed(12345)
+
+# Load data
+ppregion(xl = 0, xu = 1, yl = 0, yu = 1)
+window = owin(xrange = c(0, 1), yrange = c(0, 1))
+
+cells <- read.table("cells.dat")
+cells <- as.ppp(cells, window)
+
+# We gestimate???
+k = length(cells$x)
+phi.1 = 
+phi.2 = 
+tau.0 = 
+
+  
+  
+Stauss_sim = function(k, phi.1, phi.2, tau.0){
+  x_vec = list(x = c(), y = c())
+  
+}
 
