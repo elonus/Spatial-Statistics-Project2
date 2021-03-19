42dcc06a76da1777ac1bf4888ea657d03dac1e6b
Andreas Matre <andreas.matre@gmail.com>
2021-03-19 16:03:39 +0100

    Regner nå ut kvantiler parallellt for å spare tid.

--- Problem 4.R
+++ Problem 4.R
@@ -1,8 +1,10 @@
 library(MASS)
 library(spatstat)
 library(spatial)
+library(parallel)
 
 set.seed(12345)
+num.cores = 4
 
 # Load data
 ppregion(xl = 0, xu = 1, yl = 0, yu = 1)
@@ -39,6 +41,7 @@ pdf <- function(x, y, tau.0, phi.0, phi.1) {
 }
   
 Strauss_sim = function(k, tau.0, phi.0, phi.1, n.iter = 1000){
+  num_accept = 0
   x_vec = list(x = runif(k), y = runif(k))
   while (pdf(x_vec$x, x_vec$y, tau.0, phi.0, phi.1) == 0) {
     x_vec <- list(x = runif(k), y = runif(k))
@@ -60,33 +63,53 @@ Strauss_sim = function(k, tau.0, phi.0, phi.1, n.iter = 1000){
     alpha <- min(1, exp(-alpha.tmp))
     
     if(runif(1) <= alpha) {
+      num_accept = num_accept + 1
       x_vec$x[u] = x.prop[1]
       x_vec$y[u] = x.prop[2]
     }
   }
+  print(paste0("num_accept: ", num_accept))
   return(x_vec)
 }
 
 compute_quantiles = function(s, k, tau.0, phi.0, phi.1){
   window = owin(xrange = c(0, 1), yrange = c(0, 1))
-  sim = as.ppp(Strauss_sim(k, tau.0, phi.0, phi.1), window)
-  L_0 = Kfn(sim, fs = 1.4, k = 100)
-  L = matrix(ncol = s, nrow = length(L_0$x))
-  L[,1] = L_0$y
-  for (i in 2:s){
+  sim <- as.ppp(Strauss_sim(k, tau.0, phi.0, phi.1), window)
+  L_0 <- Kfn(sim, fs = 1.4, k = 100)
+  Ls <- mclapply(2:s, function(i)  {
     print(i)
     sim = as.ppp(Strauss_sim(k, tau.0, phi.0, phi.1), window)
-    L[,i] = Kfn(sim, fs = 1.4, k = 100)$y
-  }
-  upper_q = c()
-  lower_q = c()
-  for (i in 1:length(L_0$x)){
-    upper_q = c(upper_q, quantile(L[i,], probs = 0.95))
-    lower_q = c(lower_q, quantile(L[i,], probs = 0.05))
-  }
-  Q_mat = data.frame(x = L_0$x, upper = upper_q, lower = lower_q)
+    return(Kfn(sim, fs = 1.4, k = 100)$y)
+  },
+    mc.cores = num.cores, mc.silent = FALSE)
+  L <- matrix(unlist(Ls), byrow = FALSE, ncol = s - 1)
+  L <- cbind(L_0$y, L)
+  
+  quantiles <- apply(L, 1, function(x) quantile(x, probs = c(0.95, 0.05)))
+  Q_mat = data.frame(x = L_0$x, upper = quantiles["95%",], lower = quantiles["5%",])
   Q_mat
 }
+
+#compute_quantiles = function(s, k, tau.0, phi.0, phi.1){
+#  window = owin(xrange = c(0, 1), yrange = c(0, 1))
+#  sim = as.ppp(Strauss_sim(k, tau.0, phi.0, phi.1), window)
+#  L_0 = Kfn(sim, fs = 1.4, k = 100)
+#  L = matrix(ncol = s, nrow = length(L_0$x))
+#  L[,1] = L_0$y
+#  for (i in 2:s){
+#    print(i)
+#    sim = as.ppp(Strauss_sim(k, tau.0, phi.0, phi.1), window)
+#    L[,i] = Kfn(sim, fs = 1.4, k = 100)$y
+#  }
+#  upper_q = c()
+#  lower_q = c()
+#  for (i in 1:length(L_0$x)){
+#    upper_q = c(upper_q, quantile(L[i,], probs = 0.95))
+#    lower_q = c(lower_q, quantile(L[i,], probs = 0.05))
+#  }
+#  Q_mat = data.frame(x = L_0$x, upper = upper_q, lower = lower_q)
+#  Q_mat
+#}
 
 MC_test = function(s, dataset, k, tau.0, phi.0, phi.1, ...){  # input dataset as ppp
   quantiles = compute_quantiles(s, k, tau.0, phi.0, phi.1)
@@ -120,7 +143,7 @@ plot(res$x, res$y, type = "p")
 res <- Strauss_sim(k, tau.0 = 0.1, phi.0 = 10, phi.1 = 2, n.iter = 5000)
 plot(res$x, res$y, type = "p")
 
-res <- Strauss_sim(k, tau.0 = 0.05, phi.0 = 5, phi.1 = 20, n.iter = 5000)
+res <- Strauss_sim(k, tau.0 = 0.05, phi.0 = 5, phi.1 = 20, n.iter = 1000)
 plot(res$x, res$y, type = "p")
 MC_test(100, cells, k, tau.0 = 0.05, phi.0 = 5, phi.1 = 20)
 
